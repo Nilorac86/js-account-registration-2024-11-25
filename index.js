7d7608b8923b610d2ad81f7e706a17028313c287
Carolin Eriksson <carolineriksson@MacBook-Air-som-tillhor-Carolin.local>
2024-11-27 15:01:35 +0100

    update

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+const form = document.querySelector("form");
 const labels = document.querySelectorAll("label");
+const inputs = document.querySelectorAll("input");
+const inputPassword = document.querySelector("#password");
+const inputConfirmPassword = document.querySelector("#confirm-password")
+const submitBtn = document.querySelector("button");
+
 
 
 //##### function click one label or inputfield ######
@@ -13,4 +19,76 @@ labels.forEach ((label) => {
 }); 
 
 
+//######### checks value and validate password #######
+
+ submitBtn.disabled = true;
+
+const checkIfInputHasValue = () => {
+    const allFilled = Array.from(inputs).every(input => input.value !== "");
+
+    if (allFilled) {
+        submitBtn.disabled = false;
+    } else {
+        submitBtn.disabled = true;
+    }
+};
+
+const validatePasswords = () => {
+
+    const password = inputPassword.value;
+    const confirmPassword = inputConfirmPassword.value;
+
+     inputPassword.classList.remove("error", "valid");
+     inputConfirmPassword.classList.remove ("error", "valid");
+
+    if (password.length < 8 ){
+
+        inputPassword.classList.add("error");
+        submitBtn.disabled = true;
+    
+    } else {
+
+        inputPassword.classList.add ("valid");
+    }
+
+    if (password !== confirmPassword){
+        inputConfirmPassword.classList.add("error");
+        submitBtn.disabled=true;
+        
+        
+    } else {
+        inputConfirmPassword.classList.add("valid");
+    }
+    checkIfInputHasValue();
+};
+
+
+inputPassword.addEventListener("input", validatePasswords);
+inputConfirmPassword.addEventListener("input", validatePasswords);
+
+inputs.forEach(input => {
+    input.addEventListener("input", checkIfInputHasValue);
+});
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const nameInput = document.querySelector("#name");
+    const usernameInput = document.querySelector ("#username");
+    const emailInput = document.querySelector ("#email");
+
+
+
+const registrationData = {
+    name: nameInput.value.trim(),
+    username: usernameInput.value.trim(),
+    email: emailInput.value.trim(),
+    password: inputPassword.value,
+};
+
+console.log(registrationData);
+alert(JSON.stringify(registrationData, null, 2));
+form.reset();
+passwordInput.classList.remove("valid");
+passwordConfirmInput.classList.remove("valid");
+});
 
